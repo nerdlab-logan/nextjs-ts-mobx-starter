a1425128128ab9af2956fef65bb066892d873399
mooving-logan <logan@mooving.co.kr>
2020-10-21 18:01:04 +0900

    first commit, eslint 설정 추가, prettier 설정 추가

---
+++ src/pages/_app.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import NextApp, { AppInitialProps } from 'next/app'
+
+class App extends NextApp<AppInitialProps> {
+  render(): JSX.Element {
+    const { Component, pageProps } = this.props
+    return <Component {...pageProps} />
+  }
+}
+
+export default App
---
+++ src/pages/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const IndexPage = () => {
+  return <div>index page</div>
+}
+export default IndexPage
